efb01542a37cc0674cc841f1e122469813018ab6
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-30 23:10:27 -0300

    Programa Java que recebe uma quantidade de números e diz quantos são pares quantos são ímpares

---
+++ NumerosParesEImpares.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NumerosParesEImpares {
+    public static void main(String[] args) {
+
+        System.out.println("Este programa calcula a quantidade de números pares digitados pelo usuário. ");
+        Scanner teclado = new Scanner(System.in);
+        int numerosImparesDesejados = 0;
+        int numerosParesDesejados = 0;
+        int contador = 1;
+
+        System.out.println("Digite a quantidade de números que deseja saber: ");
+        double totalDeNumeros = teclado.nextDouble();
+
+        while (contador <= totalDeNumeros){
+            System.out.println("Digite o " + contador + "º número :");
+            double valorDigitadoPeloUsuario = teclado.nextDouble();
+
+            if (valorDigitadoPeloUsuario % 2 == 0){
+                numerosParesDesejados = numerosParesDesejados + 1;
+            }
+            if (valorDigitadoPeloUsuario % 2 == 1){
+                numerosImparesDesejados = numerosImparesDesejados + 1;
+            }
+
+            contador ++;
+        }
+        System.out.println("A quantidade de números pares é: " + numerosParesDesejados + " e a quantidade de números ímpares é: " + numerosImparesDesejados);
+    }
+}
