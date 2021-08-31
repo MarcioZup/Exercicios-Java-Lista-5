10a51243c9903d7a386687bea863aa5048b17d89
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-31 10:05:07 -0300

    Programa em Java que recebe números do usuário e diz o menor, o maior e a soma deles

---
+++ MenorEMaiorValor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MenorEMaiorValor {
+    public static void main(String[] args) {
+        System.out.println("Entre com a quantidade de números desejada: ");
+        Scanner teclado = new Scanner(System.in);
+        int quantidadeDesejada = teclado.nextInt();
+        int contador = 0;
+        int menorValor = 1;
+        int maiorValor = 0;
+
+        int numeroDigitado = 0;
+        while (contador < quantidadeDesejada){
+            System.out.println("Digite um número: ");
+            numeroDigitado = teclado.nextInt();
+            contador++;
+            if (numeroDigitado < menorValor ){
+                menorValor = numeroDigitado;
+            }
+            if (numeroDigitado >= maiorValor){
+                maiorValor = numeroDigitado;
+            }
+        }
+        System.out.println("O menor número digitado é " + menorValor + " , o maior valor digitado é " + maiorValor + " e a soma deles é " + (menorValor + maiorValor));
+
+    }
+}
